d9a7920f16767f3dcae0b7ae6e7d4553e851529d
sashulka25 <yc-user@epd649t0o2tinvqha284.auto.internal>
2023-07-29 16:15:57 +0000

    Deploy after kittygram

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,28 @@
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+        # В этой переменной будет значение для вашего проекта.
+    dsn='https://410fee3b9946448da57c77b06c789f72@o4505518229553152.ingest.sentry.io/4505518247706624',
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    traces_sample_rate=1.0,
+
+    send_default_pii=True
+)
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.136.69', '127.0.0.1', 'localhost', 'mytraining.sytes.net']
 
 
 # Application definition
@@ -103,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
