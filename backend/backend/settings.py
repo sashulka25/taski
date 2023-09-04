0973c53593e0a877e978f988a3716e2af8318d0c
Aleksandra Izhetnikova <120443917+sashulka25@users.noreply.github.com>
2023-09-04 23:03:52 +0300

    Update domain name

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.201.136.69', '127.0.0.1', 'localhost', 'mytraining.sytes.net']
+ALLOWED_HOSTS = ['84.201.136.69', '127.0.0.1', 'localhost', 'mytraining.ddns.net']
 
 
 # Application definition
